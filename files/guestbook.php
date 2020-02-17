ed4e9f88600c14e3149f780a23c6b28e5b5e83f3
arn <arnaud@desaivres.net>
2020-02-17 21:21:54 +0100

    update guestbook to work with php-redis & k8s-redis repo

--- files/guestbook.php
+++ files/guestbook.php
@@ -3,37 +3,24 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require 'Predis/Autoloader.php';
-
-Predis\Autoloader::register();
-
 if (isset($_GET['cmd']) === true) {
   $sentinel_host=getenv('REDIS_SENTINEL_SERVICE_HOST');
   $sentinel_port=getenv('REDIS_SENTINEL_SERVICE_HOST');
   $sentinel = new RedisSentinel($sentinel_host,$sentinel_port);
 
-  if (getenv('GET_HOSTS_FROM') == 'env') {
-    $host = $sentinel->getMasterAddrByName('mymaster');
-  }
-  header('Content-Type: application/json');
-  if ($_GET['cmd'] == 'set') {
-    $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host[0],
-      'port'   => $host[1],
-    ]);
+  $master = $sentinel->getMasterAddrByName('mymaster');
 
-    $client->set($_GET['key'], $_GET['value']);
+  header('Content-Type: application/json');
+
+  if ($_GET['cmd'] == 'set') {
+    $redis->connect($master[0],$master[1]);
+    $redis->set($_GET['key'], $_GET['value']);
     print('{"message": "Updated"}');
   } else {
-    $host = 'redis';
-    $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host,
-      'port'   => 6379,
-    ]);
+    $redis->connect(,getenv('REDIS_SERVICE_HOST'),)getenv('REDIS_SERVICE_PORT');
+    $redis->set($_GET['key'], $_GET['value']);
 
-    $value = $client->get($_GET['key']);
+    $value = $redis->get($_GET['key']);
     print('{"data": "' . $value . '"}');
   }
 } else {
