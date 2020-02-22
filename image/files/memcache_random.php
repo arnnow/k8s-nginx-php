8471cee687d74ae819125e33ec2b8bb5b78abeba
arn <arnaud@desaivres.net>
2020-02-22 22:34:46 +0100

    update code

--- image/files/memcache_random.php
+++ image/files/memcache_random.php
@@ -4,7 +4,7 @@ $memcache_obj = new Memcached;
 /* connect to memcached server */
 $memcache_obj->addServer(getenv('MEMCACHED_SERVICE_HOST'), getenv('MEMCACHED_SERVICE_PORT'));
 
-function getRandomWord($len = 10) {
+function getRandomWord($len = rand(1,100)) {
     $word = array_merge(range('a', 'z'), range('A', 'Z'));
     shuffle($word);
     return substr(implode($word), 0, $len);
