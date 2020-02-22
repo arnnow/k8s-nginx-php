7b6185e4285f0b19e14b86d348e858a0d1bb194b
arn <arnaud@desaivres.net>
2020-02-22 22:41:46 +0100

    update len key

--- image/files/memcache_random.php
+++ image/files/memcache_random.php
@@ -4,7 +4,7 @@ $memcache_obj = new Memcached;
 /* connect to memcached server */
 $memcache_obj->addServer(getenv('MEMCACHED_SERVICE_HOST'), getenv('MEMCACHED_SERVICE_PORT'));
 
-function getRandomWord($len = rand(1,100)) {
+function getRandomWord($len = 10) {
     $word = array_merge(range('a', 'z'), range('A', 'Z'));
     shuffle($word);
     return substr(implode($word), 0, $len);
@@ -14,8 +14,9 @@ function getRandomWord($len = rand(1,100)) {
 set value of item with key 'var_key', using on-the-fly compression
 expire time is 50 seconds
 */
+$rand = rand(1, 100);
 $key = getRandomWord();
-$memcache_obj->set($key, getRandomWord(), 50);
+$memcache_obj->set($key, getRandomWord($rand), 50);
 echo $memcache_obj->get($key);
 
 ?>
