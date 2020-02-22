d36536e19ffa0ef7967022a22070576aec3c2ee5
arn <arnaud@desaivres.net>
2020-02-22 21:20:46 +0100

    switch to memcached

--- image/files/memcache_random.php
+++ image/files/memcache_random.php
@@ -2,7 +2,7 @@
 /* OO API */
 $memcache_obj = new Memcached;
 /* connect to memcached server */
-$memcache_obj->connect(getenv('MEMCACHED_SERVICE_HOST'), getenv('MEMCACHED_SERVICE_PORT'));
+$memcache_obj->addServer(getenv('MEMCACHED_SERVICE_HOST'), getenv('MEMCACHED_SERVICE_PORT'));
 
 function getRandomWord($len = 10) {
     $word = array_merge(range('a', 'z'), range('A', 'Z'));
@@ -15,7 +15,7 @@ set value of item with key 'var_key', using on-the-fly compression
 expire time is 50 seconds
 */
 $key = getRandomWord();
-$memcache_obj->set($key, getRandomWord(), MEMCACHE_COMPRESSED, 50);
+$memcache_obj->set($key, getRandomWord(), 50);
 echo $memcache_obj->get($key);
 
 ?>
